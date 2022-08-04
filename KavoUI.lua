ffc2b0010647c5a9b5c23039d37b6ed49a3e45ab
MrDestroyerSkid <98142589+MrDestroyerSkid@users.noreply.github.com>
2022-08-04 20:21:11 +0700

    Update KavoUI.lua

--- KavoUI.lua
+++ KavoUI.lua
@@ -1,1 +1,1 @@
-game:GetService"Players".LocalPlayer:Kick("Kavo UI Library has been shutdown temporary, fixing it ASAP")
+game:GetService"Players".LocalPlayer:Kick("Kavo UI Library Remaster has been shutdown temporary, fixing it ASAP")
